6026ff887084f5df9fc5aeee222b2e48898fe92d
Nicolas Arnold <nic@roadie.io>
2021-11-24 15:51:22 +0000

    Adding in type file + ci on pull request

--- src/validator.test.js
+++ src/validator.test.js
@@ -2,15 +2,15 @@ const validator = require('./validator')
 
 describe('validator',  () => {
   it('Should successfully validate simple catalog info', async () => {
-    await expect(validator.validate('./sample/catalog-info.yml')).resolves.toBeUndefined();
+    await expect(validator.validateFromFile('./sample/catalog-info.yml')).resolves.toBeUndefined();
   });
 
   it('Should fail to validate with incorrect catalog-info', async () => {
-    await expect(validator.validate('./sample/invalid-catalog-info.yml')).rejects.toThrow();
+    await expect(validator.validateFromFile('./sample/invalid-catalog-info.yml')).rejects.toThrow();
   });
 
   it('Should successfully validate catalog info with replacements', async () => {
-    await expect(validator.validate('./sample/catalog-info-with-replacement.yml')).resolves.toBeUndefined();
+    await expect(validator.validateFromFile('./sample/catalog-info-with-replacement.yml')).resolves.toBeUndefined();
   });
 })
 
